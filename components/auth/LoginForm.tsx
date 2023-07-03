59f5da7c5427f4a7dd28f6a50e074d599b4f7158
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-03 16:43:51 +0300

    add project

---
+++ components/auth/LoginForm.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { setCookie } from "nookies";
+import styles from "./Auth.module.scss";
+import { Button, Form, Input, notification } from "antd";
+import { LoginFormDTO } from "@/api/dto/auth.dto";
+
+import * as Api from "@/api";
+
+export const LoginForm: React.FC = () => {
+  const onSubmit = async (values: LoginFormDTO) => {
+    try {
+      const { token } = await Api.auth.login(values);
+
+      notification.success({
+        message: "Успешно!",
+        description: "Go to the admin panel...",
+        duration: 2,
+      });
+
+      setCookie(null, "_token", token, {
+        path: "/",
+      });
+
+      location.href = "/dashboard";
+    } catch (err) {
+      console.warn("LoginForm", err);
+
+      notification.error({
+        message: "Error!!",
+        description: "Invalid login or password",
+        duration: 2,
+      });
+    }
+  };
+
+  return (
+    <div className={styles.formBlock}>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        onFinish={onSubmit}
+      >
+        <Form.Item
+          label="E-Mail"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Enter your email address",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Enter a password",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
